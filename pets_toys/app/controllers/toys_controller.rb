a4a7fe924a7b26526f51fa8c99be31c41e77f030
mal-wood <mallorywood413@gmail.com>
2016-11-02 10:44:58 -0700

    Implement xhr logic for ajax request

--- pets_toys/app/controllers/toys_controller.rb
+++ pets_toys/app/controllers/toys_controller.rb
@@ -3,7 +3,11 @@ class ToysController < ApplicationController
 	def index 
 		@pet = Pet.find(params[:pet_id])
 		@toys = @pet.toys
+		if request.xhr? 
+			render partial: 'toy_list', locals: {pet: @pet, toys: @toys}
+		else 
 		render json: {message: @toys}
+		end
 	end 
 
 end
