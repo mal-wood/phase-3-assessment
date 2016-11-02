dc5c13370d41365724a7d38f334033e6f5f75ab1
mal-wood <mallorywood413@gmail.com>
2016-11-02 09:34:33 -0700

    Add rails routes in routes.rb file

--- pets_toys/config/routes.rb
+++ pets_toys/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pets, except: [:create, :edit, :destroy, :update]  do 
+  	resources :toys, except: [:create, :edit, :destroy, :update] 
+  end 
 end
